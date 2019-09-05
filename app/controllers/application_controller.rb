97255b07cc1d671abfd1ea4a57ddaf3a90bb77f7
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-09-05 15:50:11 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,7 @@ class ApplicationController < Sinatra::Base
   post '/login' do
     @user = User.find_by(:username => params[:username])
       if @user != nil && @user.password == params[:password]
+        
         redirect '/account'
       else 
         erb :error 
